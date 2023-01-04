1ebe0f47518da53ad97fa4cfd36182ce13edea3a
j.fun <jmjung1997@gmail.com>
2023-01-04 23:06:13 +0900

    Programmers 크기가 작은 부분 문자열

--- 프로그래머스/크기가 작은 부분 문자열.py
+++ 프로그래머스/크기가 작은 부분 문자열.py
@@ -1,12 +1,13 @@
 def solution(t, p):
-    answer = 0
-    list=[]
-    compare=t[0:len:3]
-    print(compare)
+  answer = 0
+  p_len = len(p)
+  t_len = len(t)
+  p = int(p)
+  for i in range(0,t_len+1-p_len):
+      if int(t[i:i+p_len]) <= p:
+          answer+=1
+  return answer
 
-    return answer
 
-t=input("")
-p=int(input(""))
-print(type(t))
-solution(t,p)
+answer=solution("3141592","271")
+print(answer)
